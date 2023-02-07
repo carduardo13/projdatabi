8ca8ea046d58e964c19ea880aa50430d48fb1a41
Carlos Eduardo <carloseduardobs98@gmail.com>
2023-02-07 19:55:07 +0000

    Master

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("projdataba.controller.Main", {
+            onInit: function () {
+
+                var oDados = {
+                    pessoa: {
+                     nome: "",
+                     sobrenome: "",
+                     estado: ""
+
+                    }
+                }
+
+                var oModel = new sap.ui.model.json.JSONModel(oDados);
+                this.getView().setModel(oModel, "dados");
+
+                var oModelEstados = new sap.ui.model.json.JSONModel();
+                oModelEstados.loadData("../model/estados.json")
+                this.getView().setModel(oModelEstados, "estados")
+
+            }
+        });
+    });
